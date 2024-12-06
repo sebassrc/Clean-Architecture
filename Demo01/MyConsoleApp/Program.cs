4958392690a7a8a910963526f3db1015a145194a
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-12-05 22:42:02 -0600

    Hello Writer

---
+++ Demo01/MyConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyLibrary;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Write("Hello, Writer!");
+        }
+    }
+}
